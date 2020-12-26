d4c12379bb66e6969417e9a546c2c1a10987e0c0
NIRANJAN B <niranjannb7777@gmail.com>
2020-12-26 16:33:09 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+// Loading Something which makes this a C Program
+// Preprocessor directives
+
+int main() {
+    // data types
+//    int a;
+//    char c;
+//    float f = 4.0;
+
+    // Main Program
+//    printf("Hello World");
+
+    // format
+    // format specifier
+//    printf("The Number I stored was %d",a);
+//    printf("The Charc I stored was %c",c);
+//    printf("Enter a data for variable c: ");
+//    scanf("%c", &c);
+//    printf("Data :- %c",c);
+    // & :- addressOf, unary
+
+//    int age = 17;
+//    if (age >= 18) {
+//        printf("Y");
+//    } else {
+//        printf("N");
+//    }
+
+//    (age >= 18) ? printf("Y") : printf("N");
+    // ternary operator
+
+//    int age = 9;
+//    if (age >= 18) {
+//        printf("Y"); // greater or equal to 18
+//    } else if (age >= 10) {
+//        printf("N");  // (less than 18) or greater than 10
+//    } else {
+//        printf("O"); // less than 10
+//    }
+
+//     int month = 2;
+//     if(month == 1){
+//         printf("Jan");
+//     }else if(month == 2){
+//         printf("Feb");
+//     }else if(month == 3){
+//         printf("Mar");
+//     }else{
+//         printf("No Month");
+//     }
+
+//    switch (month){
+//         case 1: printf("Jan");
+//            break;
+//         case 2:printf("Feb");
+//            break;
+//         case 3:printf("Mar");
+//            break;
+//         default:
+//             printf("No Month");
+//     }
+
+//    /*Initialization*/
+//    while (/*Condition*/) {
+//        /* body */
+//        /*Updation*/
+//    }
+
+//    int count = 1;
+//    while (count < 4) {
+//        printf("A");
+//        count = count + 1;
+//    }
+//    printf("End :- %d\n", count);
+
+//    for(int count = 1;count < 4;count = count + 1){
+//        printf("A");
+//    }
+
+//    for(/*Initialization*/;/*Condition*/;/*Updation*/){
+//        /* body */
+//    }
+
+
+//    do{
+//        printf("A");
+//        count = count + 1;
+//    } while (count < 1);
+
+    // count = 1   A
+    // count = 2   A
+    // count = 3   A
+    // count = 4
+
+    unsigned long long int a = 6757474744545;
+
+    a = 5 + 15 / 3;
+
+    a = a + 1;
+    a += 1;
+    a++;//operand
+    // unary operator
+
+    a = 7;
+
+    return 0;
+}
