eb75e36a141822ffe9a1cbeaf4c9b3a172a7a26b
NIRANJAN B <niranjannb7777@gmail.com>
2021-01-20 17:33:46 +0530

    Create macro.c

---
+++ macro.c
@@ -0,0 +1,36 @@
+# include <stdio.h>
+
+//# define MAX 10 // Compile Time Code Generation
+// int MAX = 10 // Memory
+
+# define print_integer(x) printf("%d",x)
+# define print_string(x) printf("%s",x)
+# define print(x,y) printf(x,y)
+
+int main() {
+//    int arr[MAX];
+//    //
+//    //
+//    //
+//    int ptr[MAX];
+
+
+//    int age = 27;
+//    conditon(age, 18)
+//    {
+    print_integer(5);
+    print_string("Hello World");
+    print("%s","Hello");
+//    }
+//    conditon(age, 30)
+//    {
+//        printf("Hi");
+//    }
+//    loop(1,10){
+//        printf("%d\n",i);
+//    }
+
+
+
+
+}
