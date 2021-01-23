3c0527ef33f0dfe8245dd8a473e85c611e9cc8d7
NIRANJAN B <niranjannb7777@gmail.com>
2021-01-23 17:45:30 +0530

    Update arm.c

--- arm.c
+++ arm.c
@@ -1,32 +1,22 @@
 #include<stdio.h>
 
-// datatype def
-struct student {
-    int roll_no; // 4
-    float percentage; // 4  = 8
-};
-
-union dummy {
-    int roll_no; // 4
-    float percentage; // 4   // 4
-};
+struct demo {
+    int data;
+};  // 5
 
 void main() {
-    struct student student1;
-    union dummy dummy1;
-
-    student1.roll_no = 45;
-    student1.percentage = 90;
-
-    printf("Roll No:- %d\nPercentage:- %.2f\n",
-           student1.roll_no,
-           student1.percentage);
-
-    dummy1.roll_no = 30;
-    dummy1.percentage = 45;
-
-    printf("Roll No:- %d\nPercentage:- %.2f\n",
-           dummy1.roll_no,
-           dummy1.percentage);
-
+    struct demo value = {1};
+    struct demo *ptr = &value;
+    int a  = value;
+    printf("data :- %d\n", a);
+//    printf("char :- %c\n", value.info);
+//    printf("data :- %d\n", ptr->data);
+//    printf("char :- %c\n", ptr->info);
+//
+//    // value = *ptr
+//    // ptr = &value
+//    printf("data :- %d\n", (*ptr).data);
+//    printf("char :- %c\n", (*ptr).info);
+//    printf("data :- %d\n", (&value)->data);
+//    printf("char :- %c\n", (&value)->info);
 }
